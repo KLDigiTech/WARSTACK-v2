cec84a28e422feb4f1983c5f8150efb795ee952a
Kévin Level <kldigitech34@gmail.com>
2026-05-16 02:28:25 +0200

    fix: CORS OPTIONS + gestion salons Discord

--- bot/index.js
+++ bot/index.js
@@ -12,7 +12,8 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type, x-api-key');
-  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
   next();
 });
 app.get('/', (req, res) => res.status(200).send('WARSTACK OK'));
